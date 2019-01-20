ee07cd1223d94bf8dfb01d639ae356655cc8bdcf
Kamil Gregorczyk <gregorczyk@me.com>
2019-01-20 17:54:02 +0100

    Improves JsonBodyFilter

--- src/main/java/com/kgregorczyk/bank/filters/JsonBodyFilter.java
+++ src/main/java/com/kgregorczyk/bank/filters/JsonBodyFilter.java
@@ -14,21 +14,21 @@ import spark.Response;
 /** Verifies if incoming body is not empty and in JSON format. */
 public class JsonBodyFilter implements Filter {
 
-  private static final ImmutableSet<String> HTTP_METHODS_WITH_BODY =
-      ImmutableSet.of("POST", "PUT", "PATCH");
+  private static final ImmutableSet<String> HTTP_METHODS_WITH_BODY = ImmutableSet.of("POST", "PUT");
+
+  private static void stopRequest(String message) {
+    halt(HTTP_BAD_REQUEST, new APIResponse(Status.ERROR, message).toJson());
+  }
 
   @Override
   public void handle(Request request, Response response) {
 
     if (HTTP_METHODS_WITH_BODY.contains(request.requestMethod())) {
       if (request.body().isEmpty()) {
-        halt(HTTP_BAD_REQUEST, new APIResponse(Status.ERROR, "Body is empty").toJson());
+        stopRequest("Body is empty");
 
       } else if (!isJSONValid(request.body())) {
-        halt(
-            HTTP_BAD_REQUEST,
-            new APIResponse(Status.ERROR, "Body is required and it is not in JSON format")
-                .toJson());
+        stopRequest("Body is required and it is not in JSON format");
       }
     }
   }
--- src/test/java/com/kgregorczyk/bank/filters/JsonBodyFilterTest.java
+++ src/test/java/com/kgregorczyk/bank/filters/JsonBodyFilterTest.java
@@ -2,7 +2,6 @@ package com.kgregorczyk.bank.filters;
 
 import static com.google.common.truth.Truth.assertThat;
 import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
-import static java.net.HttpURLConnection.HTTP_CREATED;
 import static java.net.HttpURLConnection.HTTP_OK;
 
 import com.google.gson.Gson;
@@ -11,11 +10,11 @@ import com.kgregorczyk.bank.controllers.dto.APIResponse;
 import com.kgregorczyk.bank.controllers.dto.APIResponse.Status;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.junit.jupiter.api.Test;
+import spark.utils.IOUtils;
 
 class JsonBodyFilterTest extends AbstractSparkTest {
 
@@ -59,18 +58,6 @@ class JsonBodyFilterTest extends AbstractSparkTest {
   }
 
   @Test
-  public void patchMethodWithNoBodyShouldFail() throws Exception {
-    // given
-    HttpPatch request = new HttpPatch(SERVER_URL + "/api/account/createAccount");
-
-    // when
-    CloseableHttpResponse response = client.execute(request);
-
-    // assert
-    assertResponseForEmptyBody(response);
-  }
-
-  @Test
   public void postMethodWithNotValidJsonShouldFail() throws Exception {
     // given
     HttpPost request = new HttpPost(SERVER_URL + "/api/account/createAccount");
@@ -97,29 +84,34 @@ class JsonBodyFilterTest extends AbstractSparkTest {
   }
 
   @Test
-  public void patchMethodWithNotValidJsonShouldFail() throws Exception {
-    // given
-    HttpPatch request = new HttpPatch(SERVER_URL + "/api/account/createAccount");
-    request.setEntity(new StringEntity("{\"a\":}"));
-
-    // when
-    CloseableHttpResponse response = client.execute(request);
-
-    // assert
-    assertResponseForNotValidJson(response);
-  }
-
-  @Test
   public void postMethodWithValidBodyShouldNotFail() throws Exception {
     // given
     HttpPost request = new HttpPost(SERVER_URL + "/api/account");
-    request.setEntity(new StringEntity("{\"fullName\":\"Tony Stark\"}"));
+    request.setEntity(new StringEntity("{\"fullName\":\"\"}"));
 
     // when
     CloseableHttpResponse response = client.execute(request);
 
     // assert
-    assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HTTP_CREATED);
+    assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HTTP_BAD_REQUEST);
+    assertThat(IOUtils.toString(response.getEntity().getContent()))
+        .contains("There are validation errors");
+    response.close();
+  }
+
+  @Test
+  public void putMethodWithValidBodyShouldNotFail() throws Exception {
+    // given
+    HttpPut request = new HttpPut(SERVER_URL + "/api/account/123/changeFullName");
+    request.setEntity(new StringEntity("{\"fullName\":\"\"}"));
+
+    // when
+    CloseableHttpResponse response = client.execute(request);
+
+    // assert
+    assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HTTP_BAD_REQUEST);
+    assertThat(IOUtils.toString(response.getEntity().getContent()))
+        .contains("There are validation errors");
     response.close();
   }
 
